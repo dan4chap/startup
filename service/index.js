ece01f62ba9dca0bced622211ed5105443565edf
dan4chap <dan4chap@gmail.com>
2025-04-01 08:11:51 -0600

    get websocket and packages in

--- service/index.js
+++ service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const uuid = require('uuid');
 const app = express();
 const DB = require('./database.js');
+const { WebSocketServer } = require('ws');
 
 const authCookieName = 'token';
 
@@ -197,3 +198,37 @@ app.use((_req, res) => {
 const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+// CREATE A WEBSOCKET SERVER
+
+const socketServer = new WebSocketServer({ server });
+
+socketServer.on('connection', (socket) => {
+    socket.isAlive = true;
+
+    // Forward messages to everyone except the sender
+    socket.on('message', function message(data) {
+        socketServer.clients.forEach(function each(client) {
+        if (client !== socket && client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+        });
+    });
+
+    // Respond to pong messages by marking the connection alive
+    socket.on('pong', () => {
+        socket.isAlive = true;
+    });
+    });
+
+    // Periodically send out a ping message to make sure clients are alive
+    setInterval(() => {
+    socketServer.clients.forEach(function each(client) {
+        if (client.isAlive === false) return client.terminate();
+
+        client.isAlive = false;
+        client.ping();
+    });
+}, 10000);
